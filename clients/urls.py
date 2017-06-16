f4f07a6e64abdeaa9ca80ee0895e3eb280eb9673
Ilya Nikitin <amalgammus@gmail.com>
2017-06-16 13:56:13 +0400

    rebuild default view for project, add client detail view

--- clients/urls.py
+++ clients/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.clients_list, name='clients_list'),
+    url(r'^client/(?P<pk>[0-9]+)/$', views.client_detail, name='client_detail'),
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Clients
 
 def clients_list(request):
     clients = Clients.objects.all()
     return render(request, 'clients/clients_list.html', {'clients': clients})
+
+
+def client_detail(request, pk):
+    sel_client = Clients.objects.filter(client_id=pk)
+    client = get_object_or_404(Clients, pk=sel_client)
+    return render(request, 'clients/client_detail.html', {'client': client})
